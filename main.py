5e1e6fee5444089782e3af72e27a046ce5d74e33
dululu <2399029980@qq.com>
2024-01-06 19:33:08 +0800

    d

--- main.py
+++ main.py
@@ -30,6 +30,78 @@ FRIENDS_INFO_DICT = {
 }
 
 
+COOK_LABEL_LIST = [
+    "Cook",
+]
+MOVIE_LABEL_LIST = [
+    "Movie",
+]
+READ_LABEL_LIST = [
+    "Read",
+]
+DRAMA_LABEL_LIST = [
+    "Drama",
+]
+PUSHUP_LABEL_LIST = [
+    "PushUps",
+]
+BANGUMI_LABEL_LIST = [
+    "Bangumi",
+]
+GAME_LABEL_LIST = [
+    "Game",
+]
+MONEY_LABEL_LIST = [
+    "Money",
+]
+MEDITATION_LABEL_LIST = [
+    "Meditation",
+]
+MORNING_LABEL_LIST = [
+    "Morning",
+]
+GTD_LABEL_LIST = [
+    "GTD",
+]
+MY_BLOG_REPO = "dululu/notes"
+GITHUB_README_COMMENTS = (
+    "(<!--START_SECTION:{name}-->\n)(.*)(<!--END_SECTION:{name}-->\n)"
+)
+
+# add new label here
+LABEL_DICT = {
+    "Cook": {"label_list": COOK_LABEL_LIST, "comment_name": "my_cook"},
+    "Movie": {"label_list": MOVIE_LABEL_LIST, "comment_name": "my_movie"},
+    "Read": {"label_list": READ_LABEL_LIST, "comment_name": "my_read"},
+    "Drama": {"label_list": DRAMA_LABEL_LIST, "comment_name": "my_drama"},
+    "Bangumi": {"label_list": BANGUMI_LABEL_LIST, "comment_name": "my_bangumi"},
+    "Game": {"label_list": GAME_LABEL_LIST, "comment_name": "my_game"},
+}
+
+
+##### SHANBAY ######
+MY_SHANBAY_USER_NAME = "ufewz"
+SHANBAY_CALENDAR_API = "https://apiv3.shanbay.com/uc/checkin/calendar/dates/?user_id={user_name}&start_date={start_date}&end_date={end_date}"
+MY_SHANBAY_URL = f"https://web.shanbay.com/web/users/{MY_SHANBAY_USER_NAME}/zone"
+
+##### DUO ######
+MY_DUOLINGO_URL = "https://www.duolingo.com/profile/yihong0618"
+
+##### CICHANG ######
+MY_CICHANG_URL = "https://twitter.com/yihong06181/status/1359040099107897344?s=20"
+
+
+##### FOOD ######
+MY_FOOD_STAT_HEAD = (
+    "| Name | First_date | Last_date | Times | \n | ---- | ---- | ---- | ---- |\n"
+)
+MY_FOOD_STAT_TEMPLATE = "| {name} | {first_date} | {last_date} | {times} |\n"
+
+##### Month Summary ######
+MONTH_SUMMARY_HEAD = "| Month | Number | \n | ---- | ---- | \n"
+
+MONTH_SUMMARY_STAT_TEMPLATE = "| {month} | {number} |\n"
+
 def get_me(user):
     return user.get_user().login
 
@@ -336,74 +408,3 @@ if __name__ == "__main__":
     options = parser.parse_args()
     main(options.github_token, options.repo_name, options.issue_number)
 
-COOK_LABEL_LIST = [
-    "Cook",
-]
-MOVIE_LABEL_LIST = [
-    "Movie",
-]
-READ_LABEL_LIST = [
-    "Read",
-]
-DRAMA_LABEL_LIST = [
-    "Drama",
-]
-PUSHUP_LABEL_LIST = [
-    "PushUps",
-]
-BANGUMI_LABEL_LIST = [
-    "Bangumi",
-]
-GAME_LABEL_LIST = [
-    "Game",
-]
-MONEY_LABEL_LIST = [
-    "Money",
-]
-MEDITATION_LABEL_LIST = [
-    "Meditation",
-]
-MORNING_LABEL_LIST = [
-    "Morning",
-]
-GTD_LABEL_LIST = [
-    "GTD",
-]
-MY_BLOG_REPO = "yihong0618/gitblog"
-GITHUB_README_COMMENTS = (
-    "(<!--START_SECTION:{name}-->\n)(.*)(<!--END_SECTION:{name}-->\n)"
-)
-
-# add new label here
-LABEL_DICT = {
-    "Cook": {"label_list": COOK_LABEL_LIST, "comment_name": "my_cook"},
-    "Movie": {"label_list": MOVIE_LABEL_LIST, "comment_name": "my_movie"},
-    "Read": {"label_list": READ_LABEL_LIST, "comment_name": "my_read"},
-    "Drama": {"label_list": DRAMA_LABEL_LIST, "comment_name": "my_drama"},
-    "Bangumi": {"label_list": BANGUMI_LABEL_LIST, "comment_name": "my_bangumi"},
-    "Game": {"label_list": GAME_LABEL_LIST, "comment_name": "my_game"},
-}
-
-
-##### SHANBAY ######
-MY_SHANBAY_USER_NAME = "ufewz"
-SHANBAY_CALENDAR_API = "https://apiv3.shanbay.com/uc/checkin/calendar/dates/?user_id={user_name}&start_date={start_date}&end_date={end_date}"
-MY_SHANBAY_URL = f"https://web.shanbay.com/web/users/{MY_SHANBAY_USER_NAME}/zone"
-
-##### DUO ######
-MY_DUOLINGO_URL = "https://www.duolingo.com/profile/yihong0618"
-
-##### CICHANG ######
-MY_CICHANG_URL = "https://twitter.com/yihong06181/status/1359040099107897344?s=20"
-
-
-##### FOOD ######
-MY_FOOD_STAT_HEAD = (
-    "| Name | First_date | Last_date | Times | \n | ---- | ---- | ---- | ---- |\n"
-)
-MY_FOOD_STAT_TEMPLATE = "| {name} | {first_date} | {last_date} | {times} |\n"
-
-##### Month Summary ######
-MONTH_SUMMARY_HEAD = "| Month | Number | \n | ---- | ---- | \n"
-
-MONTH_SUMMARY_STAT_TEMPLATE = "| {month} | {number} |\n"
